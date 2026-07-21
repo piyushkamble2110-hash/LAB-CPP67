b4fc86c481b901f4fb0ce9eec25a86d09cf6bb6c
piyushkamble2110-hash <piyushkamble2110@gmail.com>
2026-07-21 05:23:20 +0000

    name run

--- average.cpp
+++ average.cpp
@@ -3,9 +3,9 @@ using namespace std;
 int main()
 {
     int num1,num2,sum,avg;
-    cout<<"Enter two number";
-    cin>>num1;
-    cin>>num2;
+    std::cout<<"Enter two number";
+   std::cin>>num1;
+    std::cin>>num2;
     sum=num1+num2;
     avg=sum/2;
     cout<<"Sum of two Number="<<sum<<"average="<<avg;
---
+++ name1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+namespace syc
+{
+    int show()
+    {
+    std::cout<<"My name is Piyush";
+    std::cout<<"Iam in namespace syc";
+    return 0;
+    }
+}
