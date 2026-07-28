b89c77054fe996adc5a2893d9f882c6cfb334f6c
piyushkamble2110-hash <piyushkamble2110@gmail.com>
2026-07-28 04:56:20 +0000

    commit

---
+++ class.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+class item 
+{
+    int id;
+    float cost;
+    public:
+    int getdata(int a, float b);
+    int putdata()
+    {
+        cout<<"ID of item is "<<id<<"\n";
+        cout<<"cost of item is "<<cost<<"\n";
+        return 0;
+    }
+};
+int item::getdata(int a,float b)
+{
+    id=a;
+    cost=b;
+    return 0;
+}
+int main()
+{
+    item x;
+    cout<<"object x"<<"\n";
+    x.getdata(10,35.5);
+    x.putdata();
+    item y;
+    cout<<"object y"<<"\n";
+    y.getdata(25,66.6);
+    y.putdata();
+    return 0;
+}
