19883f99993ea2bb1e10da529a1c3213b934d8ca
piyushkamble2110-hash <piyushkamble2110@gmail.com>
2026-08-04 05:31:53 +0000

    Exp2 s2,s3,s4

--- Exp2.cpp
+++ Exp2.cpp
@@ -25,10 +25,16 @@ class Student
     }
 };
 int main(){
-    Student s;
-    s.inputDetails();
-    s.displayDetails();
+    Student s1,s2,s3,s4;
+    s1.inputDetails();
+    s1.displayDetails();
+    s2.inputDetails();
+    s2.displayDetails();
+    s3.inputDetails();
+    s3.displayDetails();
+    s4.inputDetails();
+    s4.displayDetails();
     return 0;
 }
-commit
+//commit
 
