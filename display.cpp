f407a3b89af9b8351f22529fdd808dc00cb87b4a
Piyush <piyushkamble2110@gmail.com>
2026-08-08 15:23:29 +0000

    display

---
+++ display.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    cout << "(" << n << " x 1)";
+
+    return 0;
+}
+commit
